326880ee90169fe3dcbef90ed4ddd8bd33d3b9a7
dominosski <domix852@gmail.com>
2022-05-16 21:30:23 +0200

    Details screen

--- currencyCalculator/src/screens/CoinDetails/components/styles.js
+++ currencyCalculator/src/screens/CoinDetails/components/styles.js
@@ -2,19 +2,19 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     headerContainer: {
-        flexDirection: 'row', 
-        paddingHorizontal: 10, 
+        flexDirection: 'row',
+        paddingHorizontal: 10,
         alignItems: 'center',
         justifyContent: 'space-between'
     },
     tickerContainer: {
-        flexDirection:"row", 
-        alignItems:'center'
+        flexDirection: "row",
+        alignItems: 'center'
     },
-    title :{
-        color: 'white', 
-        fontWeight:'bold', 
-        marginHorizontal: 5, 
+    title: {
+        color: 'white',
+        fontWeight: 'bold',
+        marginHorizontal: 5,
         fontSize: 17
     },
     marketRank: {
@@ -22,6 +22,18 @@ const styles = StyleSheet.create({
         paddingHorizontal: 5,
         paddingVertical: 2,
         borderRadius: 5
+    },
+    currentPrice: {
+        color: 'white', fontSize: 30, fontWeight: 'bold', letterSpacing: 1,
+    },
+    name: {
+        color: 'white', fontSize: 15
+    },
+    priceContainer: {
+        padding: 15, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'
+    },
+    priceChange: {
+        color: 'white', fontSize: 17, fontWeight: '500'
     }
 });
 
--- currencyCalculator/src/screens/CoinDetails/index.jsx
+++ currencyCalculator/src/screens/CoinDetails/index.jsx
@@ -3,9 +3,12 @@ import {View, Text, Dimensions, ActivityIndicator} from 'react-native';
 import Coin from '../../../data/crypto.json';
 import DetailsHeader from "./components/DetailsHeader.jsx";
 import styles from './components/styles'
-//import {ChartDot, ChartPath, ChartPathProvider} from '@rainbow-me/animated-charts';
 import {getDetailedCoinData, getCoinMarketChart} from '../../services/requests';
 import { useNavigation } from "@react-navigation/native";
+import { AntDesign } from '@expo/vector-icons';
+//import {ChartDot, ChartPath, ChartPathProvider} from '@rainbow-me/animated-charts';
+
+
 
 const CoinDetails = () => {
     const {image:{small}, name, market_data:{market_cap_rank, current_price, price_change_percentage_24h}, symbol, prices} = Coin;
@@ -19,9 +22,30 @@ const CoinDetails = () => {
 
     const coinId = 'bitcoin';
 
+    const percentageColorChange = price_change_percentage_24h < 0 ? '#ea3943' : '#16c784';
+
+    const width = Dimensions.get('window').width;
+
+
     return(
-        <View>
-            <DetailsHeader image={small} name={name} symbol={symbol} market_cap_rank={market_cap_rank}/>
+        <View style={{paddingHorizontal: 10}}>
+                <DetailsHeader image={small} name={name} symbol={symbol} market_cap_rank={market_cap_rank}/>
+                <View style={styles.priceContainer}>
+                    <View>
+                        <Text style={styles.name}>{name}</Text>
+                        <Text style={styles.currentPrice}>${current_price.usd}</Text>ś
+                    </View>
+                    <View style={{backgroundColor: percentageColorChange, paddingHorizontal:3, paddingVertical: 10,borderRadius: 5, flexDirection: 'row'}}>
+                        <AntDesign 
+                            name={price_change_percentage_24h < 0 ? 'caretdown' : 'caretup'} 
+                            size={12} 
+                            color={'white'} 
+                            style={{alignSelf:'center', marginRight:5}}/>
+                        <Text style={styles.priceChange}>
+                            {price_change_percentage_24h.toFixed(2)}
+                        </Text>
+                    </View>
+                    </View>
         </View>
     )
     // const fetchData = async () => {
